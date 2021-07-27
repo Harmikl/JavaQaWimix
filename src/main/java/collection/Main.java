eca612443556549970c0f54e0d663aa8709c8d93
kharmak <Harmikl57@gmail.com>
2021-07-27 16:02:54 +0300

    2

--- src/main/java/collection/Main.java
+++ src/main/java/collection/Main.java
@@ -1,6 +1,8 @@
 package collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -8,13 +10,25 @@ public class Main {
         List<Person> list = new ArrayList<>();
         Person person0 = new Person("Oleg");
         Person person1 = new Person("Nikita");
+        Person person2 = new Person("Ihor");
+        Person person3 = new Person("Max");
 
         list.add(person0);
         list.add(person1);
+        list.add(person2);
+        list.add( person3);
+        Iterator<Person> iterator = list.iterator();
 
-        list.get(0);
 
-        System.out.println(list.contains(person1));
+        System.out.println(list.contains(person0));
+        System.out.println(list.get(0));
+        System.out.println(list.size());
+        System.out.println(list.indexOf(person2));
 
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(person0);
+        linkedList.add(person1);
+        linkedList.add(person2);
+        linkedList.add(person3);
     }
 }
