ce1372b93ebb6bc1b1f301f13edf5438722863c7
MaxHar <Harmikl57@gmail.com>
2021-08-07 01:52:47 +0300

    1

--- src/main/java/streams/Main.java
+++ src/main/java/streams/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         List<Staff> staff = getStaff();
 //        staff.forEach(System.out::println);
+        //reduce, mapping, count
 
         // todo Filter
        List<Staff> qa = staff.stream().filter(staff1 -> staff1.getPosition().equals(Positions.QA))
@@ -29,12 +30,12 @@ public class Main {
 
         List<Staff> sortByExperienceReversed = staff.stream().sorted(Comparator.comparing(Staff::getExperience)
                 .reversed()).collect(Collectors.toList());
-//        sortByExperienceReversed.forEach(System.out::println);
+        sortByExperienceReversed.forEach(System.out::println);
 
         //todo All match
 //         возвращает булеву переменную если совпадение по всему листу
         boolean allMatch = staff.stream().allMatch(staff1 -> staff1.getExperience() > 1.5);
-//        System.out.println(allMatch);
+        System.out.println(allMatch);
 
         // todo Any match
         //  возвращает булеву переменную если совпадение хотя бы по одному полю
@@ -50,8 +51,8 @@ public class Main {
 //                .ifPresent(System.out::println);
 
         //todo Min
-        staff.stream().min(Comparator.comparing(Staff::getExperience))
-                .ifPresent(System.out::println);
+//        staff.stream().min(Comparator.comparing(Staff::getExperience))
+//                .ifPresent(System.out::println);
 
         //
     }
